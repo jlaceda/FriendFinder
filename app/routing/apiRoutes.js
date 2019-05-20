3db6f7f2a5ae22b7885e415fad841d5742cb9860
John Laceda <john.laceda@comcast.com>
2019-05-20 02:10:10 -0700

    added best match logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,8 +9,27 @@ router.get('/api/friends', (req, res) => {
 
 router.post('/api/friends', (req, res) => {
 	const newFriend = req.body;
-	// get the number array representing the answers
-	res.json(friends);
+	const bestMatch = {
+		totalDifference: 100,
+		name: "",
+		photo: ""
+	}
+	const newFriendScores = newFriend.scores;
+	for (let i = 0; i < friends.length; i++) {
+		const friend = friends[i];
+		const scores = friend.scores;
+		let thisPersonsTotalDiff = 0;
+		for (let j = 0; j < scores.length; j++) {
+			thisPersonsTotalDiff += Math.abs(scores[j] - newFriendScores[j]);
+		}
+		if (thisPersonsTotalDiff < bestMatch.totalDifference) {
+			bestMatch.photo = friend.photo
+			bestMatch.name = friend.name;
+			bestMatch.totalDifference = thisPersonsTotalDiff;
+		}
+	}
+	friends.push(newFriend);
+	res.json(bestMatch);
 });
 
 module.exports = router;
