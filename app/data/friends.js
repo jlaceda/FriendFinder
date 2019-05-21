985d0ad50b1270136f7d92f85ee89965ebd47a77
John Michael Laceda <john.laceda@gmail.com>
2019-05-20 23:28:44 -0700

    update friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,53 @@
 const friends = [
 	{
-		name : "Ahmed",
-		photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-		scores :[
-			5,
-			1,
-			4,
-			4,
-			5,
-			1,
-			2,
-			5,
-			4,
-			1
-		]
+		name : "Emilia Clarke",
+		photo : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Emilia_Clarke_by_Gage_Skidmore_2_%28cropped%29.jpg/440px-Emilia_Clarke_by_Gage_Skidmore_2_%28cropped%29.jpg",
+		scores : [ 5, 4, 3, 2, 1, 2, 3, 4, 5, 4 ]
+	},
+	{
+		name : "Rose Leslie",
+		photo : "https://upload.wikimedia.org/wikipedia/commons/5/53/Rose_Leslie_%28March_2013%29_%28headshot%29.jpg",
+		scores : [ 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 ]
+	},
+	{
+		name : "Sean Bean",
+		photo : "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/AV0A6306_Sean_Bean.jpg/440px-AV0A6306_Sean_Bean.jpg",
+		scores : [ 2, 3, 4, 5, 1, 2, 3, 4, 5, 1 ]
+	},
+	{
+		name : "Lena Headey",
+		photo : "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Lena_Headey_Primetime_Emmy_Awards_2014.jpg/440px-Lena_Headey_Primetime_Emmy_Awards_2014.jpg",
+		scores : [ 3, 4, 5, 1, 2, 3, 4, 5, 1, 2 ]
+	},
+	{
+		name : "Maisie Williams",
+		photo : "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Maisie_Williams_Paris_Fashion_Week_Autumn_Winter_2019_%28cropped%29.jpg/440px-Maisie_Williams_Paris_Fashion_Week_Autumn_Winter_2019_%28cropped%29.jpg",
+		scores : [ 4, 5, 1, 2, 3, 4, 5, 1, 2, 3 ]
+	},
+	{
+		name : "Sophie Turner",
+		photo : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Sophie_Turner_%2840553145603%29_%28cropped%29.jpg/440px-Sophie_Turner_%2840553145603%29_%28cropped%29.jpg",
+		scores : [ 5, 1, 2, 3, 4, 5, 1, 2, 3, 4 ]
+	},
+	{
+		name : "Peter Dinklage",
+		photo : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Peter_Dinklage_by_Gage_Skidmore.jpg/440px-Peter_Dinklage_by_Gage_Skidmore.jpg",
+		scores : [ 3, 2, 3, 2, 3, 2, 3, 2, 3, 2 ]
+	},
+	{
+		name : "Kit Harington",
+		photo : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Kit_harrington_by_sachyn_mital_%28cropped%29.jpg/440px-Kit_harrington_by_sachyn_mital_%28cropped%29.jpg",
+		scores : [ 3, 2, 3, 4, 3, 2, 3, 4, 3, 2 ]
+	},
+	{
+		name : "Jason Momoa",
+		photo : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Jason_Momoa%2C_Aquaman_%2845655623114%29.jpg/440px-Jason_Momoa%2C_Aquaman_%2845655623114%29.jpg",
+		scores : [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+	},
+	{
+		name : "Kristofer Hivju",
+		photo : "https://upload.wikimedia.org/wikipedia/commons/f/f2/Kristofer_Hivju_%28Cropped%2C_2015%29.jpg",
+		scores : [ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
 	}
 ]
 module.exports = friends;
